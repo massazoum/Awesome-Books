6005b9a8c8d49cc824f2b4a5a83090ec0d5e709a
massa <zoumaniguimassa12@gmail.com>
2023-03-28 13:50:14 +0000

    Removing commits in app.js file

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-// get the book list element
-const bookList = document.getElementById('book-list');
 
-// load books from local storage or use default books
+const bookList = document.getElementById('book-list');
 
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
-// display the books in the book list element
+
 function displayBooks() {
-	// clear the book list element
-	
 
 	bookList.innerHTML = '';
-	// iterate through the books and create a list item for each book
 	books.forEach(function(book, index) {
 		const div = document.createElement('div');
 		div.innerHTML = `<div>${book.title}</div> <div> ${book.author}</div><div> <button data-index="${index}" class="remove-button">Remove</button></div><div><hr></div>`;
@@ -19,15 +14,12 @@ function displayBooks() {
 	});
 }
 
-// add a new book to the books array and display it
 function addBook(title, author) {
 	const book = {title, author};
 	books.push(book);
 	localStorage.setItem('books', JSON.stringify(books));
 	displayBooks();
 }
-
-// remove a book from the books array and display the updated list
 function removeBook(index) {
 	books = books.filter(function(book, bookIndex) {
 		return bookIndex !== index;
@@ -47,7 +39,6 @@ addBookForm.addEventListener('submit', function(event) {
 	authorInput.value = '';
 });
 
-// handle the click event on the remove button to remove a book
 bookList.addEventListener('click', function(event) {
 	if (event.target.classList.contains('remove-button')) {
 		const index = parseInt(event.target.dataset.index);
@@ -55,5 +46,4 @@ bookList.addEventListener('click', function(event) {
 	}
 });
 
-// display the initial list of books
 displayBooks();
